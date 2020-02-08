1171aaac6e676ea5e11142617d9c940df8ea78ba
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-02-08 14:32:30 +0800

    added a timeout function for the loading/spinner

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -26,30 +26,35 @@ function showStepOne(){
 	document.getElementById("step1-content").style.display = "inline";
 	document.getElementById("step2-content").style.display = "none";
 	document.getElementById("step3-content").style.display = "none";
-	document.getElementById("step4-content").style.display = "none";
+	document.getElementById("stepfour-content").style.display = "none";
 	}
 
 function showStepTwo(){
 	document.getElementById("step1-content").style.display = "none";
 	document.getElementById("step2-content").style.display = "inline";
 	document.getElementById("step3-content").style.display = "none";
-	document.getElementById("step4-content").style.display = "none";
+	document.getElementById("stepfour-content").style.display = "none";
 	}
 function showStepThree(){
 	document.getElementById("step1-content").style.display = "none";
 	document.getElementById("step2-content").style.display = "none";
 	document.getElementById("step3-content").style.display = "inline";
-	document.getElementById("step4-content").style.display = "none";
+	document.getElementById("stepfour-content").style.display = "none";
 	}
 function showStepFour(){
 	document.getElementById("step1-content").style.display = "none";
 	document.getElementById("step2-content").style.display = "none";
 	document.getElementById("step3-content").style.display = "none";
-	document.getElementById("step4-content").style.display = "inline";
+	document.getElementById("stepfour-content").style.display = "inline";
+	}
+function loadTxt(){
+	alert('Employee Successfully Onboarded!')
+	document.getElementById('acptbtn').innerHTML = 'Affirmative';
 	}
 function load(){
-	document.getElementById('acptbtn').innerHTML = 'processing..'
-	}
+	document.getElementById('acptbtn').innerHTML = '<div class="spinner-border text-success" role="status"><span class="sr-only">Loading...</span></div>'
+	setTimeout(loadTxt, 3000);
+}
 function printID(){
 	window.print()
 }
