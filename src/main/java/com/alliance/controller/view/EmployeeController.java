6bd2a679a09f5107182fba722bb8323322acae8e
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-02-11 23:02:03 +0800

    added the routes - to access -> /employee (index)

--- src/main/java/com/alliance/controller/view/EmployeeController.java
+++ src/main/java/com/alliance/controller/view/EmployeeController.java
@@ -1,8 +1,37 @@
 package com.alliance.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EmployeeController {
 
+	
+	//Onboarding 
+		
+		@RequestMapping(value ="/hrprofiler", method = RequestMethod.GET)
+		public String onBoard() {
+			return "onboarding/index";
+		}
+		
+		@RequestMapping(value = "/onboarding", method = RequestMethod.GET)
+		public String SetUpBoard(){
+			return "onboarding/onboarding";
+		}
+		
+	
+	//Employee Information Change
+		
+		@RequestMapping(value ="/emp-update", method= RequestMethod.GET)
+		public String Update() {
+			return "update/update";
+		}
+		
+	//Offboarding
+		
+		@RequestMapping(value = "/offboarding", method = RequestMethod.GET)
+		public String OffBoarding() {
+			return "offboarding/offboarding";
+		}
 }
