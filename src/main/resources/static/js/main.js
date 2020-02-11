2ad941f25ffd4f056a6368e8ad332fe0204a5433
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-02-11 23:03:07 +0800

    added the javascript for this project - made by fred

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -58,3 +58,77 @@ function load(){
 function printID(){
 	window.print()
 }
+function freds(){
+	alert("freds are")
+}
+
+function showLetterOptions(){
+	var checkbox = document.getElementById('customSwitches');
+	
+	if(checkbox.checked == true){
+		document.getElementById('txt-letter').innerHTML = 'For the employee to attach his/her resignation letter';
+		document.getElementById('txt-label-letter').innerHTML = "Still need to make a letter?";
+		document.getElementById('letter-area').style.display = "none";
+		document.getElementById('upload-area').style.display = "inline";
+	}
+	else{
+		document.getElementById('txt-letter').innerHTML = 'For the employee to write the letter addressing the company to retire';
+		document.getElementById('txt-label-letter').innerHTML = "Already have a letter?";
+		document.getElementById('letter-area').style.display = "inline";
+		document.getElementById('upload-area').style.display = "none";
+	}
+}
+
+function changeFunc() {
+    var selectBox = document.getElementById("selectBox");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    console.log(selectedValue);
+    
+    if(selectedValue == "4"){
+    	document.getElementById('add-reason').style.display = 'inline';
+    }
+    else{
+    	document.getElementById('add-reason').style.display = 'none';
+    }
+    
+}
+
+function loadTxt2(){
+	alert('Employee Successfully Offboarded!')
+	document.getElementById('acptbtn').innerHTML = 'all done';
+	}
+function loadsm(){
+	document.getElementById('acptbtn').innerHTML = '<div class="spinner-border spinner-border-sm text-light" role="status"><span class="sr-only">Loading...</span></div>'
+	setTimeout(loadTxt2, 3000);
+}
+
+//For the steppers for EMPLOYEE OFFBOARDING
+
+//STEPPER 1 - CONTENT - AND BREADCRUMBS
+function nextStep1(){
+	var element = document.getElementById("cmp1");
+	var x = document.getElementById("cmp2");
+	var step1 = document.getElementById('stepper1');
+	var step2 = document.getElementById('stepper2');
+	x.classList.remove("disabled");
+	step1.style.display = 'none';
+	step2.style.display = 'inline';
+	x.classList.add("active")
+	element.classList.add("completed");
+}
+
+//STEPPER 2 - CONTENT - AND BREADCRUMBS
+function nextStep2(){
+	var element = document.getElementById("cmp2");
+	var x = document.getElementById("cmp3");
+	var step1 = document.getElementById('stepper2');
+	var step2 = document.getElementById('stepper3');
+	x.classList.remove("disabled");
+	step1.style.display = 'none';
+	step2.style.display = 'inline';
+	x.classList.add("active")
+	element.classList.add("completed");
+}
+
+
+
